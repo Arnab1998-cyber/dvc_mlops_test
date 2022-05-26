d197af5d4092455c2c2cb571ac36ad484e5b51fd
arnab123 <arnab.kolkata98@gmail.com>
2022-05-26 10:52:51 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='src',
+      version='0.0.1',
+      description='this is a wine q package',
+      author='arnab',
+      packages=find_packages(),
+      license='MIT')
---
+++ tests/test_config.py
@@ -0,0 +1,5 @@
+def test_gen():
+    a=2
+    b=2
+    assert a==b
+
